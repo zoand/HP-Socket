bbca4074be50b4b31213c115430ceb1811f7e391
ldcsaa <ldcsaa@21cn.com>
2019-04-14 23:03:25 +0800

    20190414

--- Windows/Demo/Other Languages Demos/C#/HPSocketCS/SSLPackServer/Properties/AssemblyInfo.cs
+++ Windows/Demo/Other Languages Demos/C#/HPSocketCS/SSLPackServer/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 
 [assembly: AssemblyVersion("5.5.1.*")]
-[assembly: AssemblyFileVersion("5.5.1.4")]
+[assembly: AssemblyFileVersion("5.5.1.5")]
 [assembly: AssemblyInformationalVersion("5.5.1")]
